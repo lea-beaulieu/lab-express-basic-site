e697477869371d143210460efff8d91599fe1a18
Lea Beaulieu <beaulieu.lea@free.fr>
2021-10-30 13:45:26 +0200

    full structure, missing content and css

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 app.use(express.static('public'));
 app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/home.html'))
-app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/about.html'))
-app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/works.html'))
-app.listen(3000, () => console.log('port 3000 and structure looks ok'));
+app.get('/about', (req, res, next) => res.sendFile(__dirname + '/views/about.html'))
+app.get('/works', (req, res, next) => res.sendFile(__dirname + '/views/works.html'))
+app.listen(3000, () => console.log('port 3000 ok'));
