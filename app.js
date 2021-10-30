270c7150686fdf172032f60e98d3f22962d09f95
Lea Beaulieu <beaulieu.lea@free.fr>
2021-10-30 13:21:44 +0200

    stucture

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ app.use(express.static('public'));
 app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/home.html'))
 app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/about.html'))
 app.get('/', (req, res, next) => res.sendFile(__dirname + '/views/works.html'))
-app.listen(3000, () => console.log('port 3000 and structure looks ok'))
+app.listen(3000, () => console.log('port 3000 and structure looks ok'));
